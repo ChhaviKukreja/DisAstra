781949883f36b26bd52e813d10c6a3958bbcf928
ChhaviKukreja <chhavikukreja19@gmail.com>
2025-10-04 12:29:40 +0530

    structure updated

--- backend/src/routes/devices.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const auth = require('../middlewares/auth.middleware');
-const deviceCtrl = require('../controllers/device.controller');
-
-router.post('/', auth, deviceCtrl.registerDevice);
-
-module.exports = router;
--- backend/src/routes/messages.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/auth.middleware');
-const messageCtrl = require('../controllers/message.controller');
-
-router.post('/', auth, messageCtrl.sendMessage);
-router.get('/', auth, messageCtrl.getMessages);
-
-module.exports = router;
